b659c6730302825cdd3b3d786d3d7959bf4935dd
Abinish07 <abinishabi6@gmail.com>
2024-08-23 22:26:47 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config({ path: path.join(__dirname, ".", ".env") });
 //cors
 app.use(
   cors(
-    (origin = "https://api.abinish.in/createUser"),
+    (origin = "https://api.abinish.in/user/createUser"),
     (methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]),
     (allowedHeaders = ["Content-Type", "Authorization"])
   )
